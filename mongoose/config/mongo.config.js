f883f02db2d691c62427f3b1b200cdfef5fe6f6f
javadkavossi <105072047+javadkavossi@users.noreply.github.com>
2023-10-20 19:19:53 +0330

    update

--- mongoose/config/mongo.config.js
+++ mongoose/config/mongo.config.js
@@ -1,6 +1,10 @@
 const {default: mongoose} = require("mongoose");
 const DB_URL = "mongodb://localhost:27017/mongoose-tutorial";
-mongoose.set("strictQuery", true)
+mongoose.set("strictQuery", true)\
 mongoose.connect(DB_URL, (err) => {
+
     console.log(err ? err.message : "server connected to mongodb");
 });
+
+
+
